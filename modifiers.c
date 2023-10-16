2558c365727f58ebd3230602f6204a40b708d95f
keddo <adu.kedir.abdurahman@gmail.com>
2023-10-16 13:22:49 +0300

    Stores leading spaces to a buffer for a width modifier

--- modifiers.c
+++ modifiers.c
@@ -31,3 +31,28 @@ unsigned int print_width(buffer_t *output, unsigned int printed,
 
 	return (ret);
 }
+/**
+ * print_string_width - Stores leading spaces to a buffer for a width modifier.
+ * @output: A buffer_t struct containing a character array.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @size: The size of the string.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int prec, int size)
+{
+	unsigned int ret = 0;
+	char width = ' ';
+
+	if (NEG_FLAG == 0)
+	{
+		wid -= (prec == -1) ? size : prec;
+		for (; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
+	}
+
+	return (ret);
+}
