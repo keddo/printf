29f4af175db633fae881dfdee84ad18a55619423
keddo <adu.kedir.abdurahman@gmail.com>
2023-10-16 12:00:58 +0300

    Add handler function prototypes

--- main.h
+++ main.h
@@ -72,4 +72,13 @@ unsigned int convert_r(va_list args, buffer_t *output,
 unsigned int convert_R(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 
+
+/* Handlers */
+unsigned char handle_flags(const char *flags, char *index);
+unsigned char handle_length(const char *modifier, char *index);
+int handle_width(va_list args, const char *modifier, char *index);
+int handle_precision(va_list args, const char *modifier, char *index);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char);
+
 #endif
