3a07bab651eef7e8a9601f13867be2a2094a8768
keddo <adu.kedir.abdurahman@gmail.com>
2023-10-16 13:21:32 +0300

    add width, string width and neg-width prototypes

---
+++ modifiers.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+unsigned int print_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int prec, int size);
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
