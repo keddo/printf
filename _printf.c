4e43a9a36122e79d00285349418a0879e5e52cbc
Smats22 <tsmatsaneng@gmail.com>
2023-10-16 02:54:17 -0700

    prototype

--- _printf.c
+++ _printf.c
@@ -62,4 +62,27 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 	cleanup(args, output);
 	return (ret);
 }
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	buffer_t *output;
+	va_list args;
+	int ret;
 
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
+}
