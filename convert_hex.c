a6b581351a072b5f8b7b4bfc8d1cbe69f80876eb
keddo <adu.kedir.abdurahman@gmail.com>
2023-10-16 14:33:19 +0300

    add main.h and prototype for hex change

---
+++ convert_hex.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+unsigned int convert_x(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_X(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
