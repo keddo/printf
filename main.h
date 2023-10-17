4c2be7ec7637bb4b6693d60300188129a0cb53ed
keddo <adu.kedir.abdurahman@gmail.com>
2023-10-17 09:24:08 +0300

    fix some linting issues

--- main.h
+++ main.h
@@ -38,7 +38,6 @@ typedef struct buffer_s
 /**
  * struct converter_s - A new type defining a converter struct.
  * @specifier: A character representing a conversion specifier.
- * @int: integer
  * @func: A pointer to a conversion function corresponding to specifier.
  */
 typedef struct converter_s
